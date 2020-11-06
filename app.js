bcdc8159339b747991ba1b7f98cabbe8188b408a
MaelleDL <68912511+MaelleDL@users.noreply.github.com>
2020-11-06 10:39:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,2 @@
 var express = require("express");
 var app = express();
-
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
-
-
-// app.get("/lolilol", (req, res, next) => {
-//     json = mongo.db.find{}
-//     res.json(infos);
-// });
-
-app.get("/mdr", (req, res, next) => {
-    res.json(["Pastèque","Bernard","Michael","Ginger","Food"]);
-});
-
-app.get("/lelo", (req, res, next) => {
-    res.json(["Tony","Bernard","Autruche","Ginger","Food"]);
-});
