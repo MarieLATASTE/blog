cc7caa61181e53d665f71f6b2e17bd7b70001337
MaelleDaniel <maelledaniel2@gmail.com>
2020-11-23 08:58:01 +0100

    Commentaires

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,32 @@
+// J'appelle express:
 const express = require('express');
 const app= express();
+// J'ai fait un fichier article avec toutes mes routes dedans, je l'appelle donc:
 const artRoutes= require('./routes/article');
+// j'ai besoin de mongoose pour gérer mon type de données et communiquer avec ma bdd, je l'appelle:
 const mongoose = require('mongoose');
+// body parser sert à mettre les données dans le bon format pour la base de données, je l'appelle:
 const bodyParser= require('body-parser');
-
+// J'ai besoin de me connecter à ma base de données, j'ai fait un dossier config.json pour
+// que mon liens de bdd ne soit pas sur github, je l'ai mis dans un fichier .json que j'appelle
+// (je console log pour être bien sûre que je suis connectée):
 let monUrl = require('./config.json')
-
 mongoose.connect(monUrl.url, () => 
 console.log('connected to MongoDB!'));
 
+//  Je créer l'application body parser avec le
 app.use(bodyParser.urlencoded({ extended: false }));
-
+// J'évite les problèmes de CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
+// Je permet à mon application d'aller chercher des pages statiques dans mon dossier front end
 app.use(express.static('../frontend'));
-
+// Je fais en sorte que mon serveur "écoute sur le port de mon choix" et je le console.log pour savoir quand mon serveur est près
 app.listen(3000, () => {
     console.log('j’écoute sur le port 3000 !')});
-
+// j'indique que mon application doit utilises les routes de mon routeur
 app.use('/article',artRoutes);
--- backend/routes/article.js
+++ backend/routes/article.js
@@ -1,40 +1,40 @@
+// j'appelle express
 const express = require('express');
 const router= express.Router();
+//j'indique que mon routeur doit utiliser le modèle mongoose qui est dans article.js dans models
 const article= require('../models/article');
 
+// Toutes ce qui suit et qui commence par "router" sont des middlewares, ça indique au serveur que faire en fonction des requètes
+// qu'il reçoit de l'utilisateur. Elles sont en fonction asynchrones ce qui veut dire qu'on lui dit "essaye ça" (try) et "si t'arrive
+//pas tu fais ça" (catch).
+
+//c'est la route "principale", on dit "si on te demande ça, tu fais ça".
 router.get('/',async (req, res)=>{
     try{
+        //mais c'est quoi le "ça" qu'il doit faire?
+        //on lui dit d'aller chercher le contenu intégral de la bdd
         const arts = await article.find();
+        // et de l'envoyer au front
         res.send(arts);
     }catch (err){
+        //Si ça marche pas on envoie une erreur dans la console
+        //on pourrait aussi rediriger vers une page en écrivant un truc du genre res.redirect("la page de redirection.html")
         res.jsons({message: err});}
 });
-
+// c'est la route qui nous permet d'aller chercher les articles individuellement
 router.get('/:articleId', async (req, res) => {
     try{
+        //on luit dit quand on te demande un article par son ID, tu vas le chercher dans la bdd
         const art = await article.findById(req.params.articleId);
+        //et tu le renvois en json pour que le js du front s'en serve
         res.send(art);
     }catch(err){
+        //ici ça va toujours être le même principe: ça foire=erreur
         res.json({message: err});}
 });
-
-// router.delete('/:articleID', async(req, res) =>{
-//     try{
-//         const removedart= await article.remove({_id : req.params.articleID});
-//         res.json(removedart)
-//     }catch(err){
-//         res.json({message: err});}
-// });
-
-// router.put('/:articleID', async(req, res) =>{
-//     try{
-//         const updatedart= await article.updateOne({_id: req.params.articleID});
-//     }catch(err){
-//         res.json({message: err});
-//     }
-// });
-
+// c'est la route qui indique la marche à suivre quand le serveur reçoit un formulaire
 router.post('/', async(req,res) =>{
+     //on lui indique la "tête" des données (bodyparser fait son action ici)
     const art= new article({
     name: req.body.name,
     nickname: req.body.nickname,
@@ -44,10 +44,12 @@ router.post('/', async(req,res) =>{
     ressourceUrl: req.body.ressourceUrl,
 });
     try{
+        //on lui dit de charger tout ça dans la bdd
         await art.save();
+        //et de nous rediriger vers une page HTML fixe
         res.redirect("sommaire.html");
     }catch(err){
         res.json({message: err})}
 });
-
+//on exporte le router pour qu'il puisse être utilisé autre part (app.js)
 module.exports = router;
